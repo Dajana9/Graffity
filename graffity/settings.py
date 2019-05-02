86641089e76260624e862a32fbb7476ca5e3bada
Dajana9 <dbralic9@gmail.com>
2019-05-02 09:36:32 +0200

    a

--- graffity/settings.py
+++ graffity/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'graffity.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(
     default=os.getenv('DATABASE_URL'))
