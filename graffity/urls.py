c8e4595fb230dcca02e7a8a102b2e371c27a2d51
Dajana9 <dbralic9@gmail.com>
2019-05-02 09:49:41 +0200

    a

--- graffity/urls.py
+++ graffity/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('articles.api.urls')),
-    # re_path('.*', TemplateView.as_view(template_name='index.html')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
